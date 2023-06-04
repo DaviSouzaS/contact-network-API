7c00c6b82955a3370b6966bf88ce84583fb387c1
davi <daavisouza.santana@gmail.com>
2023-06-03 23:17:32 -0300

    feat: create client entity

---
+++ src/entities/client.entity.ts
@@ -0,0 +1,34 @@
+import { getRounds, hashSync } from "bcryptjs"
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, BeforeInsert, BeforeUpdate } from "typeorm"
+
+@Entity("client")
+export class Client {
+
+    @PrimaryGeneratedColumn("increment")
+    id: number
+
+    @Column({ length: 50 })
+    name: string
+
+    @Column({ length: 127, unique: true })
+    email: string
+
+    @Column({ length: 255 })
+    password: string
+
+    @Column({ length: 11, unique: true })
+    phone: string
+
+    @CreateDateColumn({ type: "date"})
+    created_at: string | Date 
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    hashPassword(){
+        const isEncrypted = getRounds(this.password)
+
+        if(!isEncrypted){
+            this.password = hashSync(this.password, 10)
+        }
+    }
+}
