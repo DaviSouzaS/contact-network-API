d5dcbd74de73f9fb44097288fdebb5ca9c184b86
davi <daavisouza.santana@gmail.com>
2023-06-12 09:14:56 -0300

    feat: add CORS

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import "express-async-errors"
 import express, { Application } from "express"
+import cors from "cors"
 import { handleErrors } from "./error"
 import { clientRouter } from "./routes/client/client.router"
 import { contactRouter } from "./routes/contact/contact.router"
 
 const app: Application = express()
+
+app.use(cors())
 app.use(express.json())
 
 app.use('/client', clientRouter)
